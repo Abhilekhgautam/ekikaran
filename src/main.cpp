5494aa1a42ab44301b8338f390498418e84c3755
Sagar <sagar@debian>
2023-09-29 16:19:39 +0545

    File type converted

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,15 @@ GameScreen currentScreen = GameScreen::MENU;
  int screenwidth = 850;
  int screenHeight = 450;
  InitWindow(screenwidth, screenHeight, "Ekikaran");
-
+ 
+ ToggleFullscreen();
  InitAudioDevice(); // initalize audio device
  auto  bgmMusic = LoadMusicStream("resources/coin.wav");
+ 
+ //Image first_background = LoadImage('resources/images/first_background.png');
+
+ auto first_background = LoadTexture("resources/images/first_background.png");
+ 
 
  SetMusicVolume(bgmMusic, 1.0f);
  PlayMusicStream(bgmMusic);
@@ -19,9 +25,11 @@ GameScreen currentScreen = GameScreen::MENU;
    UpdateMusicStream(bgmMusic);	 
    BeginDrawing();
     ClearBackground(WHITE);
+    DrawTexture(first_background,0,0,WHITE);
    EndDrawing();
  }
  UnloadMusicStream(bgmMusic);
+ UnloadTexture(first_background);
  CloseAudioDevice();     // Close audio context
  CloseWindow();
 

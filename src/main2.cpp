d85a543a5365709cf771c973e442e9800f16e181
Sagar <sagar@debian>
2023-10-01 08:12:48 +0545

    Merge

--- src/main2.cpp
+++ src/main2.cpp
@@ -15,6 +15,9 @@ typedef enum GameScreen
     ATTACK
 } GameScreen;
 
+GameScreen currentScreen = LOGO;
+
+
 class BaseItem : public Rectangle
 {
 public:
@@ -23,7 +26,7 @@ public:
     Texture2D texture;
     bool isObstacle;
     bool isButton;
-    bool isClickable;
+    bool isClickable=false;
     // bool isHovered;
     void draw()
     {
@@ -85,7 +88,7 @@ public:
         }
         // we will add a castle at the center of the the base
         BaseItem attackButton;
-        attackButton.texture = LoadTexture("./resources/ButtonImages/NewGameBefore.png");
+        attackButton.texture = LoadTexture("./resources/ButtonImages/attack.png");
         attackButton.x = GetScreenWidth() / 2 + 400;
         attackButton.y = -40;
         attackButton.isDraggable = false;
@@ -347,26 +350,59 @@ public:
         }
     }
 
-    BaseItem lastDraggedItem;
-    void button_clicked()
+    void click_attack()
     {
         for (auto &e : items)
         {
-            // isHovered = CheckCollisionPointRec(GetMousePosition(), {e.x, e.y, e.x + e.width, e.y + e.height});
-            if (e.isButton && GetMouseX() >= e.x && GetMouseX() <= (e.x + e.texture.width) && GetMouseY() >= e.y && GetMouseY() <= (e.y + e.texture.height) && IsMouseButtonDown(0))
+            if (e.isClickable)
             {
-                e.isBeingDragged = true;
-                lastDraggedItem = e;
-                e.drag(GetMouseX() - e.texture.width / 2, GetMouseY() - e.texture.height / 2);
+                if (CheckCollisionPointRec(GetMousePosition(), {e.x, e.y, e.x + e.width, e.y + e.height}))
+                {
+                    DrawText("Collision", 50, 100, 23, RED);
+                    if (IsMouseButtonDown(MOUSE_BUTTON_LEFT))
+                    {
+                        // DrawText("Clicked", 50, 100, 23, RED);
+                        currentScreen = ATTACK;
+                    }
+                }
             }
         }
-        if (IsMouseButtonReleased(0))
+    }
+
+    BaseItem lastDraggedItem;
+    bool button_clicked()
+    {
+        for (auto &e : items)
         {
-            BaseItem newItem;
-            newItem = lastDraggedItem; // Set your desired width
-            newItem.drag(lastDraggedItem.x, lastDraggedItem.y);
-            items.emplace_back(newItem);
+            if (e.isClickable)
+            {
+                if (IsMouseButtonDown(MOUSE_BUTTON_LEFT))
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                return false;
+            }
         }
+        // for (auto &e : items)
+        // {
+        // // isHovered = CheckCollisionPointRec(GetMousePosition(), {e.x, e.y, e.x + e.width, e.y + e.height});
+        // if (e.isButton && GetMouseX() >= e.x && GetMouseX() <= (e.x + e.texture.width) && GetMouseY() >= e.y && GetMouseY() <= (e.y + e.texture.height) && IsMouseButtonDown(0))
+        // {
+        //     e.isBeingDragged = true;
+        //     lastDraggedItem = e;
+        //     e.drag(GetMouseX() - e.texture.width / 2, GetMouseY() - e.texture.height / 2);
+        // }
+        // }
+        // if (IsMouseButtonReleased(0))
+        // {
+        //     BaseItem newItem;
+        //     newItem = lastDraggedItem; // Set your desired width
+        //     newItem.drag(lastDraggedItem.x, lastDraggedItem.y);
+        //     items.emplace_back(newItem);
+        // }
     }
 
     /*
@@ -412,184 +448,194 @@ public:
      */
 };
 
-class RivalBase{
- public:
-   RivalBase(){
-    int height = GetScreenHeight();
-    int width =  GetScreenWidth();
+class RivalBase
+{
+public:
+    RivalBase()
+    {
+        int height = GetScreenHeight();
+        int width = GetScreenWidth();
 
-    // we will always start with tiles and castle so add them to the vector initally
+        // we will always start with tiles and castle so add them to the vector initally
 
-    // add tiles to the vector
-     for(int i = -30 ; i <= height ; i += 64){
-        for(int j = -60; j <= width ; j += 64){
-	   Tile tile;
-	   tile.texture = LoadTexture("./resources/images/diagonal-grass.png");
-	   tile.x = j;
-	   tile.y = i;
-	   tile.isDraggable = false;
-	   tile.isObstacle = false;
-           items.emplace_back(tile);
-        }	    
-     }
-    // we will add a castle at the center of the the base 
-
-
-
-    BaseItem castle;
-    castle.texture= LoadTexture("./resources/images/Nuwakot.png");
-    castle.x = (int)GetScreenWidth() / 2;
-    castle.y = (int)GetScreenHeight() / 2 - 150;
-    castle.isDraggable = true;
-    castle.isObstacle = true;
-    items.emplace_back(castle);
-    castle.width = castle.texture.width;
-    castle.height = castle.texture.height;
-   
-    BaseItem camp;
-    camp.texture = LoadTexture("./resources/army-camp.png");
-    camp.x = 700;
-    camp.y = 50;
-    camp.isDraggable = true;
-    camp.isObstacle = true;
-    items.emplace_back(camp);
-    /*
-    for(int i = 200 ; i <= height / 2 + 50 ; i += 20){
-      for(int j = 630 ; j <= width / 2 + 200 ; j += 20){
-        BaseItem walls;
-	walls.texture = LoadTexture("./resources/wooden-wall.png");
-	walls.x = j;
-	walls.y = i;
-	walls.isDraggable = false;
-	walls.isObstacle = true;
-	items.emplace_back(walls);
-      }	    
-    }
-    */
-    
-
-    BaseItem mediumTree;
-    mediumTree.texture = LoadTexture("./resources/images/medum-tree.png");
-    mediumTree.x = 200;
-    mediumTree.y = 300;
-    mediumTree.isDraggable = true;  
-    mediumTree.isObstacle = true;
-    mediumTree.width = mediumTree.texture.width;
-    mediumTree.height = mediumTree.texture.height;
-    items.emplace_back(mediumTree);
-
-
-    BaseItem horse;
-    horse.texture = LoadTexture("./resources/horse-opponent.png");
-    horse.x = 300;
-    horse.y = 300;
-    horse.isDraggable = true;
-    horse.isObstacle = true;
-    items.emplace_back(horse);
-
-    BaseItem dhunga;
-    dhunga.texture = LoadTexture("./resources/large-stone.png");
-    dhunga.x = 50;
-    dhunga.y = 60;
-    dhunga.isDraggable = true;
-    dhunga.isObstacle = true;
-    items.emplace_back(dhunga);
-
-    BaseItem fallenWood;
-    fallenWood.texture = LoadTexture("./resources/large-trunk.png");
-    fallenWood.x = 490;
-    fallenWood.y = 300;
-    fallenWood.isDraggable = true;
-    fallenWood.isBeingDragged = true;
-    items.emplace_back(fallenWood);
-
-    BaseItem obDhunga;
-    obDhunga.texture = LoadTexture("./resources/obdhunga.png");
-    obDhunga.x = 340;
-    obDhunga.y = 650;
-    obDhunga.isDraggable = true;
-    obDhunga.isObstacle = true;
-    items.emplace_back(obDhunga);
-
-    BaseItem army;
-    army.texture = LoadTexture("./resources/troops.png");
-    army.x = 300;
-    army.y = 100;
-    army.isDraggable = true;
-    army.isObstacle = true;
-    items.emplace_back(army);
-/*
-    BaseItem test;
-    test.texture = LoadTexture("./resources/images/test.png");
-    test.isObstacle = true;
-    test.isDraggable = true;
-    test.posX = 200;
-    test.posY = 350;
-    items.emplace_back(test);
-    */
-   }
-   std::vector<BaseItem> items;	 
-   void draw(){
-     for(auto& elt: items){
-	      elt.draw();
-    }
-   }
-
-   bool detect_collision(BaseItem& e){
-	/*for(auto& elt: items){
-	     if(e != elt &&  elt.isObstacle &&  e.posX >= elt.posX && e.posX <= (elt.posX + elt.texture.width) && e.posY >= elt.posY && e.posY <= (elt.posY + elt.texture.height) ){
-		 return true;
-	     } // collision detected don't draw	   
-	}    
-	return false;
-   */
-    // bool collided = false;
-    for(auto elt : items){
-      // if(elt.isObstacle && CheckCollisionRecs(e, elt)) return true;
-
-      if 
-        (((e.x < (elt.x + elt.width) && e.x>elt.x ) && (e.y < (elt.y + elt.height) && e.y>elt.y)) ||
-        ((e.x < (elt.x + elt.width) && e.x>elt.x ) && (e.y + e.height < (elt.y + elt.height) && e.y + e.height>elt.y)))
-        // e.x + e.width >= elt.x && 
-        // e.x <= elt.x + elt.width &&
-        // e.y + e.height >= elt.y &&
-        // e.y <= elt.y + elt.height 
-      //   (!(e.x + e.width < elt.x || elt.x + elt.width < e.x) ||
-      //   (e.y + e.height < elt.y || elt.y + elt.height < e.y))
-      // )
+        // add tiles to the vector
+        for (int i = -30; i <= height; i += 64)
         {
-          return true;
+            for (int j = -60; j <= width; j += 64)
+            {
+                Tile tile;
+                tile.texture = LoadTexture("./resources/images/diagonal-grass.png");
+                tile.x = j;
+                tile.y = i;
+                tile.isDraggable = false;
+                tile.isObstacle = false;
+                items.emplace_back(tile);
+            }
         }
-    
-    }	   
-    return false;
-   }
-   
-   void poll_drag(){
-      for(auto& e: items){
+        // we will add a castle at the center of the the base
 
-        auto textureWidth = e.texture.width;
-	      auto textureHeight = e.texture.height;
+        BaseItem castle;
+        castle.texture = LoadTexture("./resources/images/Nuwakot.png");
+        castle.x = (int)GetScreenWidth() / 2;
+        castle.y = (int)GetScreenHeight() / 2 - 150;
+        castle.isDraggable = true;
+        castle.isObstacle = true;
+        items.emplace_back(castle);
+        castle.width = castle.texture.width;
+        castle.height = castle.texture.height;
 
-	// check for mouse drag
-	if(e.isDraggable && GetMouseX()>= e.x && GetMouseX()<= (e.x + textureWidth) && GetMouseY()>= e.y && GetMouseY() <= (e.y + textureHeight) && IsMouseButtonDown(0)){
-	   if(!detect_collision(e)){
-	     e.drag(GetMouseX()  - textureWidth/2 , GetMouseY() - textureHeight/2);
-        DrawText("No Collision", 50, 100, 23, RED);
+        BaseItem camp;
+        camp.texture = LoadTexture("./resources/army-camp.png");
+        camp.x = 700;
+        camp.y = 50;
+        camp.isDraggable = true;
+        camp.isObstacle = true;
+        items.emplace_back(camp);
+        /*
+        for(int i = 200 ; i <= height / 2 + 50 ; i += 20){
+          for(int j = 630 ; j <= width / 2 + 200 ; j += 20){
+            BaseItem walls;
+        walls.texture = LoadTexture("./resources/wooden-wall.png");
+        walls.x = j;
+        walls.y = i;
+        walls.isDraggable = false;
+        walls.isObstacle = true;
+        items.emplace_back(walls);
+          }
         }
-	   else {
-	    DrawText("Collision", 50, 100, 23, RED);	   
-	   }
-	 } 
-       }
+        */
+
+        BaseItem mediumTree;
+        mediumTree.texture = LoadTexture("./resources/images/medum-tree.png");
+        mediumTree.x = 200;
+        mediumTree.y = 300;
+        mediumTree.isDraggable = true;
+        mediumTree.isObstacle = true;
+        mediumTree.width = mediumTree.texture.width;
+        mediumTree.height = mediumTree.texture.height;
+        items.emplace_back(mediumTree);
+
+        BaseItem horse;
+        horse.texture = LoadTexture("./resources/horse-opponent.png");
+        horse.x = 300;
+        horse.y = 300;
+        horse.isDraggable = true;
+        horse.isObstacle = true;
+        items.emplace_back(horse);
+
+        BaseItem dhunga;
+        dhunga.texture = LoadTexture("./resources/large-stone.png");
+        dhunga.x = 50;
+        dhunga.y = 60;
+        dhunga.isDraggable = true;
+        dhunga.isObstacle = true;
+        items.emplace_back(dhunga);
+
+        BaseItem fallenWood;
+        fallenWood.texture = LoadTexture("./resources/large-trunk.png");
+        fallenWood.x = 490;
+        fallenWood.y = 300;
+        fallenWood.isDraggable = true;
+        fallenWood.isBeingDragged = true;
+        items.emplace_back(fallenWood);
+
+        BaseItem obDhunga;
+        obDhunga.texture = LoadTexture("./resources/obdhunga.png");
+        obDhunga.x = 340;
+        obDhunga.y = 650;
+        obDhunga.isDraggable = true;
+        obDhunga.isObstacle = true;
+        items.emplace_back(obDhunga);
+
+        BaseItem army;
+        army.texture = LoadTexture("./resources/troops.png");
+        army.x = 300;
+        army.y = 100;
+        army.isDraggable = true;
+        army.isObstacle = true;
+        items.emplace_back(army);
+        /*
+            BaseItem test;
+            test.texture = LoadTexture("./resources/images/test.png");
+            test.isObstacle = true;
+            test.isDraggable = true;
+            test.posX = 200;
+            test.posY = 350;
+            items.emplace_back(test);
+            */
     }
-   void poll_release(){
-     if(IsMouseButtonReleased(0)){
-	for(auto& e: items){
-	  e.isBeingDragged = false;	
-	}     
-     }	   
-   }
+    std::vector<BaseItem> items;
+    void draw()
+    {
+        for (auto &elt : items)
+        {
+            elt.draw();
+        }
+    }
+
+    bool detect_collision(BaseItem &e)
+    {
+        /*for(auto& elt: items){
+             if(e != elt &&  elt.isObstacle &&  e.posX >= elt.posX && e.posX <= (elt.posX + elt.texture.width) && e.posY >= elt.posY && e.posY <= (elt.posY + elt.texture.height) ){
+             return true;
+             } // collision detected don't draw
+        }
+        return false;
+       */
+        // bool collided = false;
+        for (auto elt : items)
+        {
+            // if(elt.isObstacle && CheckCollisionRecs(e, elt)) return true;
+
+            if (((e.x < (elt.x + elt.width) && e.x > elt.x) && (e.y < (elt.y + elt.height) && e.y > elt.y)) ||
+                ((e.x < (elt.x + elt.width) && e.x > elt.x) && (e.y + e.height < (elt.y + elt.height) && e.y + e.height > elt.y)))
+            // e.x + e.width >= elt.x &&
+            // e.x <= elt.x + elt.width &&
+            // e.y + e.height >= elt.y &&
+            // e.y <= elt.y + elt.height
+            //   (!(e.x + e.width < elt.x || elt.x + elt.width < e.x) ||
+            //   (e.y + e.height < elt.y || elt.y + elt.height < e.y))
+            // )
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void poll_drag()
+    {
+        for (auto &e : items)
+        {
+
+            auto textureWidth = e.texture.width;
+            auto textureHeight = e.texture.height;
+
+            // check for mouse drag
+            if (e.isDraggable && GetMouseX() >= e.x && GetMouseX() <= (e.x + textureWidth) && GetMouseY() >= e.y && GetMouseY() <= (e.y + textureHeight) && IsMouseButtonDown(0))
+            {
+                if (!detect_collision(e))
+                {
+                    e.drag(GetMouseX() - textureWidth / 2, GetMouseY() - textureHeight / 2);
+                    DrawText("No Collision", 50, 100, 23, RED);
+                }
+                else
+                {
+                    DrawText("Collision", 50, 100, 23, RED);
+                }
+            }
+        }
+    }
+    void poll_release()
+    {
+        if (IsMouseButtonReleased(0))
+        {
+            for (auto &e : items)
+            {
+                e.isBeingDragged = false;
+            }
+        }
+    }
 };
 
 int main(void)
@@ -600,7 +646,6 @@ int main(void)
     InitWindow(screenWidth, screenHeight, "Ekikaran");
     ToggleFullscreen();
 
-    GameScreen currentScreen = LOGO;
 
     int framesCounter = 0; // Useful to count frames
     int framesCounter1 = 0;
@@ -697,8 +742,9 @@ int main(void)
 
     // initialization for strategy
     auto KPImage = LoadImage("resources/images/characters/kalupandey.png");
-    ImageResize(&KPImage, 1500, 750);
+    ImageResize(&KPImage, 2500, 1000);
     auto KPTexture = LoadTextureFromImage(KPImage);
+    // auto KPTexture = LoadTexture("resources/images/characters/kalupandey.png");
     auto PNSImage = LoadImage("resources/images/characters/PNSedited.png");
     ImageFlipHorizontal(&PNSImage);
     ImageResize(&PNSImage, GetScreenWidth(), GetScreenHeight() - 100);
@@ -788,7 +834,7 @@ int main(void)
             IsHoveredNew = CheckCollisionPointRec(GetMousePosition(), (Rectangle){(float)(GetScreenWidth() / 2 - 65), (float)(GetScreenHeight() / 2 - 165), 220, 85});
             IsHoveredSetting = CheckCollisionPointRec(GetMousePosition(), (Rectangle){(float)(GetScreenWidth() / 2 - 65), (float)(GetScreenHeight() / 2 - 15), 220, 85});
             IsHoveredQuit = CheckCollisionPointRec(GetMousePosition(), (Rectangle){(float)(GetScreenWidth() / 2 - 65), (float)(GetScreenHeight() / 2 + 135), 220, 85});
-            IsHoveredAttack = CheckCollisionPointRec(GetMousePosition(), (Rectangle){(GetScreenWidth()/1.0- 390.0), 15.0, GetScreenWidth()/1.0-15.0,80});
+            IsHoveredAttack = CheckCollisionPointRec(GetMousePosition(), (Rectangle){(GetScreenWidth() / 1.0 - 390.0), 15.0, GetScreenWidth() / 1.0 - 15.0, 80});
             // LoadMenu(FirstBackgroundImage);
             DrawTexture(FirstBackgroundTexture, 0, 0, WHITE); // Draw Background
             if (exitWindowRequested)
@@ -945,10 +991,11 @@ int main(void)
             {
                 Entered = true;
                 transAlpha = 0.0f;
-            }else{
+            }
+            else
+            {
                 DrawTexture(skyTexture, 0, 0, WHITE);
                 DrawTextPro(font, TextSubtext(text, 0, framesCounter / 10), {10, 190}, {0, 0}, 0, 25, 1.0, BLACK);
-
             }
             if (Entered)
             {
@@ -988,8 +1035,8 @@ int main(void)
             BeginDrawing();
             ClearBackground(WHITE);
             DrawTexture(skyTexture, 0, 0, WHITE);
-            DrawTexture(PNSTexture, -500, 200, WHITE);
-            DrawTexture(KPTexture, 170, 200, WHITE);
+            DrawTexture(PNSTexture, -800, 220, WHITE);
+            DrawTexture(KPTexture, -700, 50, WHITE);
             DrawTexture(speechTextureForPNS, 130, 190, WHITE);
             DrawTexture(speechTextureForKP, 650, 230, WHITE);
             if (textCounter < 300)
@@ -1047,7 +1094,7 @@ int main(void)
         {
             my_base.draw();
             my_base.poll_drag();
-            my_base.button_clicked();
+            my_base.click_attack();
             if (IsKeyPressed(KEY_ENTER))
             {
                 Entered = true;
@@ -1072,19 +1119,19 @@ int main(void)
         {
             rival_base.draw();
             rival_base.poll_drag();
-            if (IsHoveredAttack)
-            {
-                DrawTexture(NewAfter, (GetScreenWidth() / 2 - 120), (GetScreenHeight() / 2 - 240), WHITE);
-                if (IsMouseButtonDown(MOUSE_BUTTON_LEFT))
-                {
-                    Entered = true;
-                    transAlpha = 0.0f;
-                }
-            }
-            else
-            {
-                DrawTexture(NewBefore, (GetScreenWidth()/1.0- 390.0), (GetScreenWidth()/1.0-15.0), WHITE);
-            }
+            // if (IsHoveredAttack)
+            // {
+            //     DrawTexture(NewAfter, (GetScreenWidth() / 2 - 120), (GetScreenHeight() / 2 - 240), WHITE);
+            //     if (IsMouseButtonDown(MOUSE_BUTTON_LEFT))
+            //     {
+            //         Entered = true;
+            //         transAlpha = 0.0f;
+            //     }
+            // }
+            // else
+            // {
+            //     DrawTexture(NewBefore, (GetScreenWidth()/1.0- 390.0), (GetScreenWidth()/1.0-15.0), WHITE);
+            // }
             if (IsKeyPressed(KEY_ENTER))
             {
                 Entered = true;
